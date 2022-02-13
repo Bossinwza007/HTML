13eaca7f57c48a9d1f33aeda818c69e3b26adccf
Bossinwza007 <6306021620026@fitm.kmutnb.ac.th>
2022-02-13 15:14:13 +0700

    update week4

---
+++ week1/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <link rel="stylesheet" href="eiei.css">
+  <title>เเนะนำตัวเอง</title>
+ </head>
+ <body background="12b23a7752e8a7a4464c1ff5e596237f.gif">
+  <?php
+  echo "
+  <h2><b><marquee scrollamount=\"15\" direction=\"left\" ><u>เเนะนำตัวเอง</u></marquee></h2>
+  <div class=\"img\"></div>
+  <p>ชื่อ : กษม ตรีพืช</p>
+  <p>Name : Kasama Treepuech</p>
+  <p>รหัส 6306021620026</p>
+  <p>ภาควิชาเทคโนโลยีสารสนเทศ คณะเทคโนโลยีเเละการจัดการอุตสาหกรม</p>
+  <p>มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ วิทยาเขตปราจีนบุรี</b></p>
+  <h4>Facebook : <a href=\"https://www.facebook.com/kasama.treepuech.9/\" target=\"_blank\">Kasama Treepuech</a>  Instragram : <a href=\"https://www.instagram.com/crazyboss.19/\" target=\"_blank\"><font color=\"yellow\">Crazyboss.19</font></a></h4>
+  <table border=\"1\"  align=\"center\">
+    <colgroup span=\"2\" width=\"150\"></colgroup>
+    <colgroup width=\"250\"></colgroup>
+    <thead>
+        <tr height=\"100px\" align=\"center\">
+            <th>ลำดับ</th>
+            <th width=\"200\">ละดับชั้น</th>
+            <th width=\"500\">โรงเรียน</th>
+            <th width=\"200\">จังหวัด</th>
+        </tr>
+    </thead>
+      <tr align=\"center\" >
+        <th>1</th>
+        <th>มัธยมศึกษา</th>
+        <th>โรงเรียนชลราษฎรอำรุง</th>
+        <th>ชลบุรี</th>
+      </tr>
+      <tr align=\"center\" >
+        <td>2</td>
+        <td>อุดมศึกษา</td>
+        <td>มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</td>
+        <th>ปราจีนบุรี</th>
+      </tr>
+    </table>
+<br>
+    <table border=\"1\"  align=\"center\">
+      <colgroup span=\"2\" width=\"150\"></colgroup>
+      <thead>
+        <tr height=\"50px\" align=\"center\">
+          <th>ลำดับ</th>
+          <th>งานอดิเรก</th>
+        </tr>
+      </thead>
+        <tr>
+        <td>1</td>
+        <td>เล่นเกม</td>
+        </tr>
+        <tr>
+        <td>2</td>
+        <td>ดูหนัง</td>
+        </tr>
+        <tr>
+        <td>3</td>
+        <td>ดูการ์ตูน</td>
+        </tr>
+        <tr>
+        <td>4</td>
+        <td>อ่านมังงะ</td>
+        </tr>
+      </table>
+      <br><br>
+";
+?>
+</body>
+</html>
