74e6b0ebe2e9e232da917be43d811f836971a8ff
Bossinwza007 <6306021620026@fitm.kmutnb.ac.th>
2022-05-09 16:16:48 +0700

    update

--- final/ข้อ1เเละ2/detailBook.php
+++ final/ข้อ1เเละ2/detailBook.php
@@ -16,12 +16,26 @@
     }else{
         $image = "<img src=\"pictures/".$data["picture"]."\" align =\"center\" width=\"200\" height =\"200\" style=\"padding:30px\">";
     }
+    $sql2 = "SELECT * FROM typebook ORDER BY typeId";
+    $dbquery1 = mysqli_query($conn,$sql2);
+    $sql3 = "SELECT * FROM statusbook ORDER BY statusId";
+    $dbquery2 = mysqli_query($conn,$sql3);
+    while($result= mysqli_fetch_array($dbquery1)){ 
+        if($result["typeId"] == $data["typeId"]){
+            $type = $result["typeName"];
+        }
+    }
+    while($result= mysqli_fetch_array($dbquery2)){ 
+        if($result["statusId"] == $data["statusId"]){
+            $status = $result["statusName"];
+        }
+    }
     echo "<table border=1 align=center bgcolor=#FFCCCC>";
     echo "<tr><td align=center colspan = 2bgcolor =#FF99CC><B>แสดงรายละเอียดหนังสือ</B></td></tr>";
     echo "<tr><td> รหัสหนังสือ : </td><td>".$data["bookId"]."</td></tr>";
     echo "<tr><td> ชื่อหนังสือ  : </td><td>".$data["bookName"]."</td></tr>";
-    echo "<tr><td> ประเภทหนังสือ  : </td><td>".$data["typeId"]."</td></tr>";
-    echo "<tr><td> สถานะหนังสือ   : </td><td>".$data["statusId"]."</td></tr>";
+    echo "<tr><td> ประเภทหนังสือ  : </td><td>".$type."</td></tr>";
+    echo "<tr><td> สถานะหนังสือ   : </td><td>".$status."</td></tr>";
     echo "<tr><td> สำนักพิมพ์  : </td><td>".$data["publish"]."</td></tr>";
     echo "<tr><td> ราคาซื้อ  : </td><td>".$data["unitPrice"]."</td></tr>";
     echo "<tr><td> ราคาเช่า  : </td><td>".$data["unitRent"]."</td></tr>";
